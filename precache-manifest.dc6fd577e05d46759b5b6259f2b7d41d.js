06c5319ff7ba61995ae806d5f2ded0c7fbdbdcc8
Kamil <kjstepaniak1@gmail.com>
2019-03-15 08:39:14 +0000

    Updates

---
+++ precache-manifest.dc6fd577e05d46759b5b6259f2b7d41d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b4e70f137e814e68b3c7",
+    "url": "/static/js/main.2f844f94.chunk.js"
+  },
+  {
+    "revision": "809818fbb852a7c27512",
+    "url": "/static/js/2.d10dbc86.chunk.js"
+  },
+  {
+    "revision": "b4e70f137e814e68b3c7",
+    "url": "/static/css/main.689da3a1.chunk.css"
+  },
+  {
+    "revision": "809818fbb852a7c27512",
+    "url": "/static/css/2.1863c7ff.chunk.css"
+  },
+  {
+    "revision": "ff17901b79a7f38c35afdd1768d5886c",
+    "url": "/index.html"
+  }
+];
